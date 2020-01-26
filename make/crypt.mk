b7164000a1d8c3a1db03b9bb61c4a3d170f43d53
Jeroen Visser <jeroen.visser@schiphol.nl>
2020-01-26 21:58:33 +0100

    It's the year 2020 now

--- make/crypt.mk
+++ make/crypt.mk
@@ -2,7 +2,8 @@
 CRYPT_SECRET ?= .ansible-vault-password
 
 # Ansible Vault command shortcut
-ANSIBLE_VAULT = ansible-vault --vault-id $(CRYPT_SECRET)
+ANSIBLE_VAULT_ARGS = --vault-id $(CRYPT_SECRET)
+ANSIBLE_VAULT = ansible-vault $(ANSIBLE_ACTION)
 
 # This will decrypt any .env targets automatically (requires there is a .env.enc available).
 %/.env: %/.env.enc | $(CRYPT_SECRET)
@@ -16,16 +17,27 @@ $(CRYPT_SECRET):
 	echo $$response > $@ && \
 	exec <&-
 
+SECRETHUB_ARGS ?=
+# Shortcuts for invoking the secrethub binaries
+SECRETHUB_DOCKER_ARGS ?=
+SECRETHUB = docker run \
+	-v $HOME/.secrethub:/root/.secrethub \
+	-v $(ROOT_DIR):/workspace \
+	--workdir /workspace \
+	$(KUBECTL_DOCKER_ARGS) \
+	secrethub/cli \
+	$(KUBECTL_ARGS)
+
 # Helpers
 
 __DECRYPT = \
 	$(info Decrypting $< to $@) \
-	$(ANSIBLE_VAULT) decrypt --output $@ $<
+	$(ANSIBLE_VAULT) decrypt $(ANSIBLE_VAULT_ARGS) --output $@ $<
 
 __CRYPT = \
 	$(info Encrypting $< to $@) \
-	$(ANSIBLE_VAULT) encrypt --output $@ $<
+	$(ANSIBLE_VAULT) encrypt $(ANSIBLE_VAULT_ARGS) --output $@ $<
 
 .PHONY: entrypoint-ansible-vault
 entrypoint-ansible-vault: $(CRYPT_SECRET)
-	$(ANSIBLE_VAULT) $(ARGS)
+	$(ANSIBLE_VAULT) $(ANSIBLE_VAULT_ARGS) $(ARGS)
